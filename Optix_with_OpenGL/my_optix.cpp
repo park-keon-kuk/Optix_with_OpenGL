ceba0ba94ee1df316bd88fbb28c467121cade659
park-keon-kuk <pkk1113@naver.com>
2018-09-19 23:54:57 +0900

    .

--- Optix_with_OpenGL/my_optix.cpp
+++ Optix_with_OpenGL/my_optix.cpp
@@ -6,7 +6,20 @@ MyOptix::MyOptix()
 	if (m_context)
 		return;
 
+	RTprogram pinhole_camera = nullptr;
+	RTprogram thin_lens_camera = nullptr;
+	RTprogram exception = nullptr;
+
 	rtContextCreate(&m_context);
+	rtContextSetEntryPointCount(m_context, 2);
+	rtContextSetPrintEnabled(m_context, 1); // 0 is disable, otherwise enable.
+	rtContextSetPrintBufferSize(m_context, 4096);
+
+	rtContextSetRayGenerationProgram(m_context, 0, pinhole_camera);
+	rtContextSetRayGenerationProgram(m_context, 1, thin_lens_camera);
+
+	rtContextSetExceptionProgram(m_context, 0, exception);
+	rtContextSetExceptionProgram(m_context, 1, exception);
 }
 
 MyOptix::~MyOptix()
